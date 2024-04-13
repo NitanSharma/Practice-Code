4567fe7c81e02160821be808173382a3d489298b
NitanSharma <sharmanitan938@gmail.com>
2024-04-13 10:34:20 +0530

    Sum Program

---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+    public static void main(String[] args) {
+        int x=5;
+        int y=10;
+
+        int result = x+y;
+        System.out.println(result);
+    }
+    
+}
